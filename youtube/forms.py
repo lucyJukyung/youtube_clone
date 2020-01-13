4b9ce4dc4c8fad71e284e457626c2aa68898efb1
Lucy.H <june9014@gmail.com>
2020-01-13 17:37:18 +1100

    added login session/file upload/nav bar

--- youtube/forms.py
+++ youtube/forms.py
@@ -2,10 +2,15 @@ from django import forms
 
 
 class LoginForm(forms.Form):
-    username = forms.CharField(label='Your name', max_length=20)
-    password = forms.CharField(label='Your Password', max_length=20)
+    username = forms.CharField(label='Username', max_length=20)
+    password = forms.CharField(label='Password', max_length=20)
 
 class RegisterForm(forms.Form):
-    username = forms.CharField(label='Your name', max_length=20)
-    password = forms.CharField(label='Your Password', max_length=20)
+    username = forms.CharField(label='Username', max_length=20)
+    password = forms.CharField(label='Password', max_length=20)
     email = forms.CharField(label='Email', max_length=20)
+
+class NewVideoForm(forms.Form):
+    title = forms.CharField(label='Title', max_length=20)
+    description = forms.CharField(label='Description', max_length=100)
+    path = forms.FileField()
--- youtube/views.py
+++ youtube/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 from django.views.generic.base import View, HttpResponse, HttpResponseRedirect
-from .forms import LoginForm, RegisterForm
+from .forms import LoginForm, RegisterForm, NewVideoForm
 from django.contrib.auth.models import User
-# Create your views here.
+from django.contrib.auth import authenticate, login, logout
 
 class HomeView(View):
     template_name = "index.html"
@@ -15,18 +15,48 @@ class LoginView(View):
     template_name = "login.html"
 
     def get(self, request):
+        if request.user.is_authenticated:
+            # Do something for authenticated users.
+            print("already logged in. redirecting")
+            print(request.user) #to see who is logged in.
+            logout(request)
+            return HttpResponseRedirect('new_video')
+
         form = LoginForm()
         return render(request, self.template_name, {'form': form})
 
     def post(self, request):
         # print(request)
         # print(dir(request))
-        return HttpResponse("This is Index view. POST Request.")
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+
+            #need a special function bc psswrd is encrypted in db
+            user = authenticate(request, username=username, password=password)
+
+            if user is not None:
+                login(request, user) #this login creates the cookie for user to stay logged in.
+                #request will have a value (user) inside.
+                print("authentication success")
+                # or create a new entry in a table to store logs
+                return HttpResponseRedirect('HomeView')
+            else:
+                print("authentication failed")
+                return HttpResponseRedirect('login')
+                #if login fails, redirect to login page
+        return HttpResponse('This is login view. POST request.')
 
 class RegisterView(View):
     template_name = "register.html"
 
     def get(self, request):
+        if request.user.is_authenticated:
+            # Do something for authenticated users.
+            print("already logged in. redirecting")
+            print(request.user) #to see who is logged in.
+            return HttpResponseRedirect('new_video')
         form = RegisterForm()
         return render(request, self.template_name, {'form': form})
 
@@ -59,8 +89,9 @@ class RegisterView(View):
 class NewVideo(View):
     template_name = "new_video.html"
     def get(self, request):
+        form = NewVideoForm()
         variableA = "New videos"
-        return render(request, self.template_name, {'variableA': variableA})
+        return render(request, self.template_name, {'form': form})
 
     def post(self, request):
         return HttpResponse("This is Index view. POST Request.")
